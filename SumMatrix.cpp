413bca6a5c0ece8e90c0c26462506871334fbf54
kevincwong6 <48312245+kevincwong6@users.noreply.github.com>
2019-08-15 00:26:36 -0700

    Create SumMatrix.cpp
    Provide a matrix a.
    Create a new matrix b.
    Each element  in b is the sum of matrix a of all left side and above elements.
    For example:
    
    b[0][2]=a[0][0]+a[0][1]+a[0][2]
    b[1][2] = a[0][0]+a[0][1]+a[0][2]+a[1][0]+a[1][1]+a[1][2]

---
+++ SumMatrix.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+void printM(string msg, vector<vector<int>> m)
+{
+    int rowCnt = m.size();
+    int colCnt = m[0].size();
+
+    cout << msg << ":" << endl;
+    for (int i = 0; i < rowCnt; i++)
+    {
+        for (int j = 0; j < colCnt; j++)
+        {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+vector<vector<int>> SumMatrix(vector<vector<int>> orig) {
+    vector<vector<int>> res = orig; // use copy constructor to initialize res with all values from orig
+    int rowCnt = orig.size();
+    int colCnt = orig[0].size();
+
+    for (int i = 1; i < colCnt; i++) // prepare for the first row
+    {   // the first result row = previous result col + curr col from the original table
+        res[0][i] = res[0][i - 1] + orig[0][i];
+    }
+
+    for (int i = 1; i < rowCnt; i++) // prepare for the first col
+    {
+        res[i][0] = res[i - 1][0] + orig[i][0];
+    }
+
+    // initialize col sum with the first row values with copy constructor
+    // it is used to hold the col sum up to previous row
+    vector<int> colSum(orig[0]);
+
+    for (int rowIdx = 1; rowIdx < rowCnt; rowIdx++) // process each row starting from row idx 1
+    {
+        for (int colIdx = 1; colIdx < colCnt; colIdx++) // process each row starting from col idx 1
+        {
+            // sum = curr row of previous col +
+            //       the col sum that has the total value up to previous row +
+            //       the curr col/row value from the orig matrix
+            int sum = res[rowIdx][colIdx - 1] + colSum[colIdx] + orig[rowIdx][colIdx];
+
+            // update col sum that will be used for next row
+            colSum[colIdx] = colSum[colIdx]+orig[rowIdx][colIdx];
+
+            res[rowIdx][colIdx] = sum; // update result matrix
+        }
+    }
+   return res;
+}
+
+void testCaseSumMatrix(vector<vector<int>> a)
+{
+    vector<vector<int>> b = SumMatrix(a);
+
+    printM("input", a);
+    printM("output", b);
+}
+
+void UnitTestSumMatrix()
+{
+    vector<vector<int>> a{ {1, 2, 3}, {3,4, 5} };
+    testCaseSumMatrix(a);
+
+    a = { {3, 3, 3},  {3, 3, 3},  {3, 3, 3} };
+    testCaseSumMatrix(a);
+}
+
+int main()
+{
+    UnitTestSumMatrix();
+
+    return 0;
+}
